69b3723ec34097f55c4ca4423b5f1525468bad95
Ahmed <ahmedealsalman@gmail.com>
2020-02-06 14:40:18 +0200

    cashier task is done

--- cashier.py
+++ cashier.py
@@ -1,5 +1,27 @@
 def main():
-	# write code here
+	listOfItems = []
+	dict = {"name":"", "price":"", "quantity":""}
+	total = 0
+	while dict["name"] != "done":
+		dict = {"name":"", "price":"", "quantity":""}
+		dict["name"] = input('Item (enter "done" when finished): ')
+		if dict["name"] == "done":
+			break
+		dict["price"] = float(input("Price: "))
+		dict["quantity"] = int(input("Quantity: "))
+		total += (float(dict["price"]) * dict["quantity"])
+		listOfItems.append(dict)
+
+	print("""-------------------
+receipt
+-------------------""")
+	for item in listOfItems:
+		print(f'{item["quantity"]} {item["name"]} {float(item["price"]) * int(item["quantity"])}JD')
+
+	print("-------------------")
+	print(f"Total Price: {total}JD")
+
+
 
 if __name__ == '__main__':
 	main()
